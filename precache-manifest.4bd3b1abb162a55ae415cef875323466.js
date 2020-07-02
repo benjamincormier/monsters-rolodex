4722618ed5189c73763a1bc70fe3febdd92cbd49
Benjamin Cormier <benjamin.cormier@outlook.com>
2020-07-02 23:00:42 +0200

    Updates

---
+++ precache-manifest.4bd3b1abb162a55ae415cef875323466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db2a510a8177a129747e1b689fd5b19",
+    "url": "/benjamincormier/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c5123a0106a813aa8b22",
+    "url": "/benjamincormier/monsters-rolodex/static/css/main.5fc0fde1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/benjamincormier/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/benjamincormier/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5123a0106a813aa8b22",
+    "url": "/benjamincormier/monsters-rolodex/static/js/main.ff461504.chunk.js"
+  },
+  {
+    "revision": "a451abd98a8c333ce755",
+    "url": "/benjamincormier/monsters-rolodex/static/js/runtime-main.bc35b170.js"
+  }
+]);
